eeca8c58d7d1f6d25ec6d53fd81d7db144255903
varungarg2796 <varungarg2796@hotmail.com>
2018-07-24 00:35:58 +0530

    Initial files

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+const publicPath = path.join(__dirname, '../public') // .. --> means going up a directory (so we go to ChatAppNode)
+const port = process.env.PORT || 3000;
+const express = require("express")
+const app = express()
+
+
+
+app.use('/', express.static(publicPath)); // opens index.html in public
+
+app.listen(port, ()=>{
+    console.log("Server started at 3000")
+})
